6554612d146c410cbebacc7497b3f8e60e297a14
Aabhas Senapati <aabhas2211@gmail.com>
2020-10-03 19:59:45 +0530

    Update Final.ino

--- Final.ino
+++ Final.ino
@@ -1,5 +1,4 @@
 
-
 #include <WiFi.h>
 #include <FirebaseESP32.h>
 
@@ -11,7 +10,6 @@
 DHT dht(DHTPIN, DHTTYPE);
 
 
-int arr[10000];
 int Signal;   
 int PulseSensorPurplePin = 32;  
 #define FIREBASE_HOST "https://teamrio-backend.firebaseio.com/"
@@ -22,6 +20,12 @@ int PulseSensorPurplePin = 32;
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 0;
 const int   daylightOffset_sec = 3600;
+FirebaseJsonArray arr;
+
+void printResult(FirebaseData &data);
+
+
+unsigned long sendDataPrevMillis = 0;
 //Define FirebaseESP32 data object
 FirebaseData firebaseData;
 FirebaseJson json;
@@ -125,27 +129,207 @@ void loop()
   //this may need revision
   //in example.ino not sure how firebseData gets initialized
 
-  for(int i =0 ; i <10000 ; i++) 
-  {
-  
-    Signal = analogRead(PulseSensorPurplePin);
-          arr[i] = Signal;
-    Serial.println(arr[i]);
-    delay(10);
+
+   // if (millis() - sendDataPrevMillis > 15000)
+    //{
+       // sendDataPrevMillis = millis();
+       // count++;
+
+        Serial.println("------------------------------------");
+        Serial.println("Set Array...");
+
+        arr.clear();
+
+       
+        for (int i =0 ;i<1000;i++)
+        {
+          String str = "/[";
+          str+=String(i);
+          str+="]";
+           Signal = analogRead(PulseSensorPurplePin);
+          arr.set(str,Signal);
+           delay(10);
+        }  
+        if (Firebase.set(firebaseData, path + "/Data", arr))
+        {
+            Serial.println("PASSED");
+            Serial.println("PATH: " + firebaseData.dataPath());
+            Serial.println("TYPE: " + firebaseData.dataType());
+            Serial.print("VALUE: ");
+            printResult(firebaseData);
+            Serial.println("------------------------------------");
+            Serial.println();
+        }
+        else
+        {
+            Serial.println("FAILED");
+            Serial.println("REASON: " + firebaseData.errorReason());
+            Serial.println("------------------------------------");
+            Serial.println();
+        }
+
+        Serial.println("------------------------------------");
+        Serial.println("Get Array...");
+        if (Firebase.get(firebaseData,  path + "/Data"))
+        {   
+            Serial.println("PASSED");
+            Serial.println("PATH: " + firebaseData.dataPath());
+            Serial.println("TYPE: " + firebaseData.dataType());
+            Serial.print("VALUE: ");
+            printResult(firebaseData);               
+            Serial.println("------------------------------------");
+            Serial.println();
+        }
+        else
+        {
+            Serial.println("FAILED");
+            Serial.println("REASON: " + firebaseData.errorReason());
+            Serial.println("------------------------------------");
+            Serial.println();
+        }
+    //}
+
+//    if (!Firebase.readStream(firebaseData))
+//    {
+//        Serial.println("------------------------------------");
+//        Serial.println("Can't read stream data...");
+//        Serial.println("REASON: " + firebaseData.errorReason());
+//        Serial.println("------------------------------------");
+//        Serial.println();
+//    }
+
+    if (firebaseData.streamTimeout())
+    {
+        Serial.println("Stream timeout, resume streaming...");
+        Serial.println();
+    }
+
+    if (firebaseData.streamAvailable())
+    {
+        Serial.println("------------------------------------");
+        Serial.println("Stream Data available...");
+        Serial.println("STREAM PATH: " + firebaseData.streamPath());
+        Serial.println("EVENT PATH: " + firebaseData.dataPath());
+        Serial.println("DATA TYPE: " + firebaseData.dataType());
+        Serial.println("EVENT TYPE: " + firebaseData.eventType());
+        Serial.print("VALUE: ");
+        printResult(firebaseData);
+        Serial.println("------------------------------------");
+        Serial.println();
     }
   try{
      
     Firebase.setFloat(firebaseData, path + "/Temperature", t); //TEMPERATURE data in Celcius
   
-    for(int i =0 ; i <10000 ; i++) 
-  {
- 
-    Serial.println(arr[i]);
-     Firebase.setInt(firebaseData,path + "/pulse/"+String(i), arr[i]); 
-  
-    }
+
   }
    catch(int e){
     Serial.println("ERROR: Exception no: " + e );
   }
 }
+void printResult(FirebaseData &data)
+{
+
+    if (data.dataType() == "int")
+        Serial.println(data.intData());
+    else if (data.dataType() == "float")
+        Serial.println(data.floatData(), 5);
+    else if (data.dataType() == "double")
+        printf("%.9lf\n", data.doubleData());
+    else if (data.dataType() == "boolean")
+        Serial.println(data.boolData() == 1 ? "true" : "false");
+    else if (data.dataType() == "string")
+        Serial.println(data.stringData());
+    else if (data.dataType() == "json")
+    {
+        Serial.println();
+        FirebaseJson &json = data.jsonObject();
+        //Print all object data
+        Serial.println("Pretty printed JSON data:");
+        String jsonStr;
+        json.toString(jsonStr, true);
+        Serial.println(jsonStr);
+        Serial.println();
+        Serial.println("Iterate JSON data:");
+        Serial.println();
+        size_t len = json.iteratorBegin();
+        String key, value = "";
+        int type = 0;
+        for (size_t i = 0; i < len; i++)
+        {
+            json.iteratorGet(i, type, key, value);
+            Serial.print(i);
+            Serial.print(", ");
+            Serial.print("Type: ");
+            Serial.print(type == FirebaseJson::JSON_OBJECT ? "object" : "array");
+            if (type == FirebaseJson::JSON_OBJECT)
+            {
+                Serial.print(", Key: ");
+                Serial.print(key);
+            }
+            Serial.print(", Value: ");
+            Serial.println(value);
+        }
+        json.iteratorEnd();
+    }
+    else if (data.dataType() == "array")
+    {
+        Serial.println();
+        //get array data from FirebaseData using FirebaseJsonArray object
+        FirebaseJsonArray &arr = data.jsonArray();
+        //Print all array values
+        Serial.println("Pretty printed Array:");
+        String arrStr;
+        arr.toString(arrStr, true);
+        Serial.println(arrStr);
+        Serial.println();
+        Serial.println("Iterate array values:");
+        Serial.println();
+        for (size_t i = 0; i < arr.size(); i++)
+        {
+            Serial.print(i);
+            Serial.print(", Value: ");
+
+            FirebaseJsonData &jsonData = data.jsonData();
+            //Get the result data from FirebaseJsonArray object
+            arr.get(jsonData, i);
+            if (jsonData.typeNum == FirebaseJson::JSON_BOOL)
+                Serial.println(jsonData.boolValue ? "true" : "false");
+            else if (jsonData.typeNum == FirebaseJson::JSON_INT)
+                Serial.println(jsonData.intValue);
+            else if (jsonData.typeNum == FirebaseJson::JSON_FLOAT)
+                Serial.println(jsonData.floatValue);
+            else if (jsonData.typeNum == FirebaseJson::JSON_DOUBLE)
+                printf("%.9lf\n", jsonData.doubleValue);
+            else if (jsonData.typeNum == FirebaseJson::JSON_STRING ||
+                     jsonData.typeNum == FirebaseJson::JSON_NULL ||
+                     jsonData.typeNum == FirebaseJson::JSON_OBJECT ||
+                     jsonData.typeNum == FirebaseJson::JSON_ARRAY)
+                Serial.println(jsonData.stringValue);
+        }
+    }
+    else
+    {
+        Serial.println(data.payload());
+    }
+}
+
+
+
+
+
+  
+
+
+
+  
+  
+
+ 
+
+
+
+
+ 
+
+
